d6409bc5dcdacc2ce4e53e155931bbaeaf0abe51
Todd Kulesza <todd@dropline.net>
2015-08-01 10:58:51 -0700

    Intelligently format dates and times.

--- src/net/straylightlabs/archivo/view/RecordingDetailsController.java
+++ src/net/straylightlabs/archivo/view/RecordingDetailsController.java
@@ -27,9 +27,26 @@ import javafx.scene.image.ImageView;
 import net.straylightlabs.archivo.model.Recording;
 
 import java.net.URL;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.ResourceBundle;
 
+// TODO Only show original air date when it differs from recorded on date
+// TODO Generalize recorded on date formatting for use in the RecordingList view
+// TODO Duration should let the user know the show is still recording unless it is completed
+// FIXME TiVo Suggestions header item continues to show details of last selected item
+// FIXME Movies keep showing the image of the last selected item
+
 public class RecordingDetailsController implements Initializable {
+    private final static DateTimeFormatter DATE_RECORDED_LONG_DATE_FORMATTER;
+    private final static DateTimeFormatter DATE_RECORDED_SHORT_DATE_FORMATTER;
+    private final static DateTimeFormatter DATE_RECORDED_TIME_FORMATTER;
+    private final static DateTimeFormatter DATE_AIRED_FORMATTER;
+    private final static int currentYear;
+    private final static int currentDay;
+
     @FXML
     private Label title;
     @FXML
@@ -48,6 +65,15 @@ public class RecordingDetailsController implements Initializable {
     private Label description;
     @FXML
     private ImageView image;
+
+    static {
+        DATE_RECORDED_LONG_DATE_FORMATTER = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM);
+        DATE_RECORDED_SHORT_DATE_FORMATTER = DateTimeFormatter.ofPattern("MMM d");
+        DATE_RECORDED_TIME_FORMATTER = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
+        DATE_AIRED_FORMATTER = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM);
+        currentYear = LocalDateTime.now().getYear();
+        currentDay = LocalDateTime.now().getDayOfYear();
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -96,19 +122,24 @@ public class RecordingDetailsController implements Initializable {
     private void showRecordingDetails(Recording recording) {
         setLabelText(title, recording.getSeriesTitle());
         setLabelText(subtitle, recording.getEpisodeTitle());
+        setLabelText(description, recording.getDescription());
 
-        setLabelText(episode, recording.getSeasonAndEpisode());
-        setLabelText(date, recording.getDateRecorded().toString());
-        if (recording.getOriginalAirDate() != null)
-            setLabelText(originalAirDate, "Originally aired on " + recording.getOriginalAirDate().toString());
+        setLabelText(date, formatDateTime(recording.getDateRecorded()));
+        if (recording.getDuration() != null)
+            setLabelText(duration, formatDuration(recording.getDuration()));
         if (recording.getChannel() != null)
             setLabelText(channel, recording.getChannel().toString());
-        if (recording.getDuration() != null)
-            setLabelText(duration, recording.getDuration().toString());
-        setLabelText(description, recording.getDescription());
-        if (recording.getImageURL() != null)
+
+        setLabelText(episode, recording.getSeasonAndEpisode());
+        if (recording.getOriginalAirDate() != null) {
+            setLabelText(originalAirDate, "Originally aired on " +
+                    recording.getOriginalAirDate().format(DATE_AIRED_FORMATTER));
+        }
+
+        if (recording.getImageURL() != null) {
             image.setImage(new Image(recording.getImageURL().toString(),
                     Recording.DESIRED_IMAGE_WIDTH, Recording.DESIRED_IMAGE_HEIGHT, true, true, true));
+        }
     }
 
     private void setLabelText(Label label, String text) {
@@ -121,5 +152,51 @@ public class RecordingDetailsController implements Initializable {
             label.setVisible(false);
             label.setManaged(false);
         }
+    }
+
+    private static String formatDateTime(LocalDateTime dateTime) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Recorded ");
+
+        if (dateTime.getDayOfYear() == currentDay) {
+            sb.append("today");
+        } else if (dateTime.getDayOfYear() == currentDay - 1) {
+            sb.append("yesterday");
+        } else if (dateTime.getYear() == currentYear) {
+            // Don't include the year for recordings from the current year
+            sb.append(dateTime.format(DATE_RECORDED_SHORT_DATE_FORMATTER));
+        } else {
+            sb.append(dateTime.format(DATE_RECORDED_LONG_DATE_FORMATTER));
+        }
+
+        sb.append(" at ");
+        sb.append(dateTime.format(DATE_RECORDED_TIME_FORMATTER));
+
+        return sb.toString();
+    }
+
+    private static String formatDuration(Duration duration) {
+        int hours = (int) duration.toHours();
+        int minutes = (int) duration.toMinutes() - (hours * 60);
+        int seconds = (int) (duration.getSeconds() % 60);
+
+        // Round so that we're only displaying hours and minutes
+        if (seconds >= 30) {
+            minutes++;
+        }
+        if (minutes >= 60) {
+            hours++;
+            minutes = 0;
+        }
+
+        String formatted;
+        if (hours > 0) {
+            formatted = String.format("%d:%02d hour", hours, minutes);
+            if (hours > 1 || minutes > 0)
+                formatted += "s";
+        } else {
+            formatted = String.format("%d minutes", minutes);
+        }
+        return formatted;
     }
 }
